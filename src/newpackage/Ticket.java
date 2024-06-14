a3ebddbcb291f2b6ab0a6cbfc921eaa44f19a3d6
Camila Martín <camilamartin1403@gmail.com>
2024-06-13 22:58:22 -0300

    create ticket class

---
+++ src/newpackage/Ticket.java
@@ -0,0 +1,53 @@
+package newpackage;
+
+import java.util.Date;
+
+public class Ticket {
+    
+    private int number;
+    private int row;
+    private int seat;
+    private double price;
+    private Date purchaseDate;
+    private Date validityDate;
+    
+    public Ticket(){
+        
+    }
+
+    public Ticket(int number, int row, int seat, double price, Date purchaseDate, Date validityDate) {
+        this.number = number;
+        this.row = row;
+        this.seat = seat;
+        this.price = price;
+        this.purchaseDate = purchaseDate;
+        this.validityDate = validityDate;
+    }
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+    
+}
---
+++ src/newpackage1/CinemaP2.java
@@ -0,0 +1,8 @@
+package newpackage1;
+
+public class CinemaP2 {
+    public static void main(String[] args) {
+        
+    }
+    
+}
