e7ecc57f6e4b5b9b4f2b5166e452c586d56e6001
Camila Martín <camilamartin1403@gmail.com>
2024-06-13 23:23:19 -0300

    add representative comments

--- src/newpackage1/CinemaP2.java
+++ src/newpackage1/CinemaP2.java
@@ -7,21 +7,16 @@ import newpackage.Ticket;
 
 public class CinemaP2 {
     
-    
-    
-    
-    
-    
-    
-    
-    
     public static void main(String[] args) {
         
+        //create data structure
         List<Ticket> ticketList = new ArrayList<Ticket>();
         
+        //create tickets
         Ticket ticket1 = new Ticket (12, 2, 3, 1500, new Date(), new Date());
         Ticket ticket2 = new Ticket (13, 2, 4, 1500, new Date(), new Date());
 
+        //create ticket using empty constructor and apply encapsulation
         Ticket ticket3 = new Ticket();
         ticket3.setNumber(20);
         ticket3.setRow(5);
@@ -30,6 +25,10 @@ public class CinemaP2 {
         ticket3.setPurchaseDate(new Date());
         ticket3.setValidityDate(new Date());
         
+        //add elements to the list
+        ticketList.add(ticket1);
+        ticketList.add(ticket2);
+        ticketList.add(ticket3);
         
         
         
